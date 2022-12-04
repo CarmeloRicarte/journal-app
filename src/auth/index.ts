9a88835995ff53456a129654e602d6738659e02c
Carmelo Ricarte Rocamora <cricarterocamora@icloud.com>
2022-12-04 12:18:16 +0100

    feat: create auth layout, login and register page

--- src/auth/index.ts
+++ src/auth/index.ts
@@ -1,2 +1,3 @@
-export * from './pages';
-export * from './routes/AuthRoutes';
+export * from "./layout";
+export * from "./pages";
+export * from "./routes/AuthRoutes";
--- src/auth/pages/RegisterPage.tsx
+++ src/auth/pages/RegisterPage.tsx
@@ -1,5 +1,50 @@
+import { Button, Grid, Link, TextField, Typography } from "@mui/material";
+import { Link as RouterLink } from "react-router-dom";
+import { AuthLayout } from "../layout";
 export const RegisterPage = () => {
   return (
-    <div>RegisterPage</div>
-  )
-}
+    <AuthLayout title="Create an account">
+      <form>
+        <Grid container>
+          <Grid item xs={12} sx={{ mt: 2 }}>
+            <TextField
+              label="Full Name"
+              type="text"
+              placeholder="Your full name"
+              fullWidth
+            />
+          </Grid>
+          <Grid item xs={12} sx={{ mt: 2 }}>
+            <TextField
+              label="Email"
+              type="email"
+              placeholder="email@gmail.com"
+              fullWidth
+            />
+          </Grid>
+          <Grid item xs={12} sx={{ mt: 2 }}>
+            <TextField
+              label="Password"
+              type="password"
+              placeholder="Password"
+              fullWidth
+            />
+          </Grid>
+          <Grid container spacing={2} sx={{ my: 1 }}>
+            <Grid item xs={12}>
+              <Button type="button" variant="contained" fullWidth>
+                Sign up
+              </Button>
+            </Grid>
+          </Grid>
+          <Grid container direction="row" justifyContent="end">
+            <Typography sx={{ mr: 1 }}>Have you an account?</Typography>
+            <Link component={RouterLink} color="inherit" to="/auth/login">
+              Log in
+            </Link>
+          </Grid>
+        </Grid>
+      </form>
+    </AuthLayout>
+  );
+};
