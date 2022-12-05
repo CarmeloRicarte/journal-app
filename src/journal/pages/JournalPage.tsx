ac59a5626666b68b198c416502c3490ca9bc1754
Carmelo Ricarte Rocamora <cricarterocamora@icloud.com>
2022-12-05 20:34:04 +0100

    feat: add floating button

--- src/journal/pages/JournalPage.tsx
+++ src/journal/pages/JournalPage.tsx
@@ -1,12 +1,20 @@
+import { Fab } from "@mui/material";
+import { Add } from "@mui/icons-material";
 import { JournalLayout } from "../layout/JournalLayout";
 import { NothingSelectedView } from "../views";
 import { NoteView } from "../views";
-
 export const JournalPage = () => {
   return (
     <JournalLayout>
       {/*<NothingSelectedView />*/}
       <NoteView />
+      <Fab
+        color="primary"
+        aria-label="add"
+        sx={{ position: "fixed", right: 50, bottom: 10 }}
+      >
+        <Add />
+      </Fab>
     </JournalLayout>
   );
 };
