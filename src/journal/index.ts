cc159c092c90db4aa3846a8ceec26e1e63ff2d38
Carmelo Ricarte Rocamora <cricarterocamora@icloud.com>
2022-12-05 20:01:12 +0100

    feat: create view for nothing selected and add it to page

--- src/journal/index.ts
+++ src/journal/index.ts
@@ -2,3 +2,4 @@ export * from "./components";
 export * from "./layout";
 export * from "./pages/JournalPage";
 export * from "./routes/JournalRoutes";
+export * from "./views";
--- src/journal/pages/JournalPage.tsx
+++ src/journal/pages/JournalPage.tsx
@@ -1,10 +1,11 @@
 import { Typography } from "@mui/material";
 import { JournalLayout } from "../layout/JournalLayout";
+import { NothingSelectedView } from "../views";
 
 export const JournalPage = () => {
   return (
     <JournalLayout>
-      <Typography>JournalPage</Typography>
+      <NothingSelectedView />
     </JournalLayout>
   );
 };
