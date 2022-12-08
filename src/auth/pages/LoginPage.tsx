48a4901cfdf44c4bc0ccc2d895c08be0eb03376a
Carmelo Ricarte Rocamora <cricarterocamora@icloud.com>
2022-12-08 11:56:29 +0100

    feaet: add functionality to buttons for sign in dispatching actions

--- src/auth/pages/LoginPage.tsx
+++ src/auth/pages/LoginPage.tsx
@@ -1,11 +1,33 @@
 import { Google } from "@mui/icons-material";
 import { Button, Grid, Link, TextField, Typography } from "@mui/material";
 import { Link as RouterLink } from "react-router-dom";
+import { useForm } from "../../hooks";
+import { useAppDispatch } from "../../store/hooks";
 import { AuthLayout } from "../layout";
+import {
+  checkingAuthentication,
+  startGoogleSignIn,
+} from "../../store/auth/thunks";
 export const LoginPage = () => {
+  const dispatch = useAppDispatch();
+  const { email, password, onInputChange } = useForm({
+    email: "carmelo@google.com",
+    password: "123456",
+  });
+
+  const onSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    dispatch(checkingAuthentication({ email, password }));
+  };
+
+  const onGoogleSignIn = (event: React.MouseEvent<HTMLButtonElement>) => {
+    event.preventDefault();
+    dispatch(startGoogleSignIn({ email, password }));
+  };
+
   return (
     <AuthLayout title="Login">
-      <form>
+      <form onSubmit={onSubmit}>
         <Grid container>
           <Grid item xs={12} sx={{ mt: 2 }}>
             <TextField
@@ -13,6 +35,9 @@ export const LoginPage = () => {
               type="email"
               placeholder="email@gmail.com"
               fullWidth
+              name="email"
+              value={email}
+              onChange={onInputChange}
             />
           </Grid>
           <Grid item xs={12} sx={{ mt: 2 }}>
@@ -21,16 +46,24 @@ export const LoginPage = () => {
               type="password"
               placeholder="Password"
               fullWidth
+              name="password"
+              value={password}
+              onChange={onInputChange}
             />
           </Grid>
           <Grid container spacing={2} sx={{ my: 1 }}>
             <Grid item xs={12} sm={6}>
-              <Button type="button" variant="contained" fullWidth>
+              <Button type="submit" variant="contained" fullWidth>
                 Sign in
               </Button>
             </Grid>
             <Grid item xs={12} sm={6}>
-              <Button type="button" variant="contained" fullWidth>
+              <Button
+                type="button"
+                onClick={onGoogleSignIn}
+                variant="contained"
+                fullWidth
+              >
                 <Google />
                 <Typography sx={{ ml: 1 }}>Google</Typography>
               </Button>
