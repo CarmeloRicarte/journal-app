684e34bcb95b6e9eae2db864293f98d3b1ed4dc3
Carmelo Ricarte Rocamora <cricarterocamora@icloud.com>
2022-12-10 14:49:27 +0100

    test: delete image after tests of fileUpload.ts

--- __tests__/journal/helpers/fileUpload.test.ts
+++ __tests__/journal/helpers/fileUpload.test.ts
@@ -1,6 +1,16 @@
-import { fileUpload } from "../../../src/journal/helpers";
 import { describe, test, expect } from "vitest";
 import axios from "axios";
+import { v2 as cloudinary } from "cloudinary";
+import { fileUpload } from "../../../src/journal/helpers";
+import { getEnvironments } from "../../../src/helpers";
+
+const { VITE_CLOUD_NAME, VITE_API_KEY, VITE_API_SECRET } = getEnvironments();
+cloudinary.config({
+  cloud_name: VITE_CLOUD_NAME,
+  api_key: VITE_API_KEY,
+  api_secret: VITE_API_SECRET,
+  secure: true,
+});
 
 describe("Tests of fileUpload", () => {
   test("should upload file successfully to Cloudinary", async () => {
@@ -9,7 +19,14 @@ describe("Tests of fileUpload", () => {
     const { data } = await axios.get(imageUrl, { responseType: "arraybuffer" });
     const file = new File([data], "foto.jpg");
     const url = await fileUpload(file);
+
     expect(typeof url).toBe("string");
+
+    const segments = url.split("/");
+    const imageId = segments[segments.length - 1].replace(".jpg", "");
+    await cloudinary.api.delete_resources([`journal-app/${imageId}`], {
+      resource_type: "image",
+    });
   });
 
   test("should return null", async () => {
