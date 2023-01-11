8957658eebfb7a0413ec7d100e34fa6d6640c685
Carmelo Ricarte Rocamora <cricarterocamora@icloud.com>
2023-01-11 17:13:34 +0100

    refactor: modify how to get env variables

--- src/helpers/getEnvironments.ts
+++ src/helpers/getEnvironments.ts
@@ -1,6 +1,12 @@
 export const getEnvironments = () => {
-  import.meta.env;
   return {
-    ...import.meta.env,
+    VITE_FIREBASE_APIKEY: import.meta.env.VITE_FIREBASE_APIKEY,
+    VITE_FIREBASE_AUTH_DOMAIN: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+    VITE_FIREBASE_PROJECT_ID: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+    VITE_FIREBASE_STORAGE_BUCKET: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+    VITE_FIREBASE_MESSAGING_SENDER_ID: import.meta.env
+      .VITE_FIREBASE_MESSAGING_SENDER_ID,
+    VITE_FIREBASE_APP_ID: import.meta.env.VITE_FIREBASE_APP_ID,
+    VITE_CLOUDINARY_URL: import.meta.env.VITE_CLOUDINARY_URL,
   };
 };
